74fc915f9135ac376fdf9a1380cca081d7b4b7e2
lesnikovskiy <lesnikovski@gmail.com>
2024-08-10 23:03:17 +0300

    use &mut [String] (slice annotation) to be more flexible

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ fn print_elements(elements: &[String]) {
     elements.iter().for_each(|el| println!("{}", el))
 }
 
-fn short_strings(elements: &mut Vec<String>) {
+fn short_strings(elements: &mut [String]) {
     elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
 fn main() {
     let mut colors = vec![String::from("red"), String::from("green"), String::from("blue")];
 
-    short_strings(&mut colors);
+    short_strings(&mut colors[0..2]);
     print_elements(&colors);
 }
