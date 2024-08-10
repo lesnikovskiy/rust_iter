21f6da7fb073b5d7d6c3b1c1300b4dd8a7b06bb0
lesnikovskiy <lesnikovski@gmail.com>
2024-08-10 22:47:54 +0300

    Well we can use &Vec in slice argument

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,5 @@ fn print_elements(elements: &[String]) {
 fn main() {
     let colors = vec![String::from("red"), String::from("green"), String::from("blue")];
 
-    print_elements(&colors[1..3]);
+    print_elements(&colors);
 }
