835853e456571a3814fb5703b05b155cfbe70a4a
lesnikovskiy <lesnikovski@gmail.com>
2024-08-10 22:59:08 +0300

    shorten_string function as demo to work with iter_mut()

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 fn print_elements(elements: &[String]) {
-    elements
-        .iter()
-        .map(|el| format!("{} {}", el, el))
-        .for_each(|el| println!("{}", el))
+    elements.iter().for_each(|el| println!("{}", el))
+}
+
+fn short_strings(elements: &mut Vec<String>) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
 fn main() {
-    let colors = vec![String::from("red"), String::from("green"), String::from("blue")];
+    let mut colors = vec![String::from("red"), String::from("green"), String::from("blue")];
 
+    short_strings(&mut colors);
     print_elements(&colors);
 }
